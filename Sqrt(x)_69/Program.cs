24cb9f933ed0a88450bc4b793828cd1007eb26f1
msimsek <mahmut5206@gmail.com>
2024-03-18 22:15:22 +0300

    sdfdsfd

--- Sqrt(x)_69/Program.cs
+++ Sqrt(x)_69/Program.cs
@@ -4,19 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(  MySqrt(50) );
+            Console.WriteLine(  MySqrt(2147483647) );
         }
         static int MySqrt(int x)
         {
             int deger = 0;
-          
+            if (x==0)
+            {
+                return 0;
+            }
+             if (x == 1)
+            {
+                return 1;
+            }
+             if (x == 2)
+            {
+                return 1;
+            }
+
             for (int i = 0; i < x; i++)
             {
-                if (i*i>x)
+                int ikare = i * i;
+                if ((ikare < x)&&((i+1)*(i+1))>x)
+                {
+                    //
+                    deger = i;
+                    break;
+                   
+                }
+                if ( Math.BigMul(i,i) > x)
                 {
                     deger = i-1;
                     break;
                 }
+                if (ikare == x)
+                {
+                    deger = i ;
+                    break;
+                }
+                
             }
             return deger;
         }
